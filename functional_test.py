b6026459e6adde0ac81fcbfc71808c864139d7d1
Jason Tumusiime <jason.2mcme@gmail.com>
2024-03-06 11:33:49 +0300

    Modify FT checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,10 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,18 +23,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the a page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholer'), 'Enter a to-do item')
 
-        # She enter "Buy peackock feathers" into a textbox (Edith's hobby is tying 
+        # She enters "Buy peackock feathers" into a textbox (Edith's hobby is tying 
         # fly-fishing lures)
+        inputbox.send_keys("Buy peackock feathers")
 
         # When she hits enter, the page updates, and now the page lists
-        # "1. By peacock feathers" as an item on the to-do list
+        # "1. Buy peacock feathers" as an item on the to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element('id_list_table')
+        rows = self.browser.find_elements('tr', By.TAG_NAME)
+        self.assertTrue(
+            any(row.text == "1. Buy peacock feathers" for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She enters "Use
         # peacock feahers to make a fly" (Edith is very methodical)
+        self.fail('Finish the test!')
 
         # Edith wonders whether the site remembers her list. Then she sees a that the
         # site has generated a URL for her -- there is some explanatory text to that 
