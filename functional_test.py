8a9aecf41178e8f255a5de01a0d1c4a511f4df87
Jason Tumusiime <jason.2mcme@gmail.com>
2024-03-06 13:40:08 +0300

    Add form to post new to-do item to list

--- functional_test.py
+++ functional_test.py
@@ -33,7 +33,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She enters "Buy peackock feathers" into a textbox (Edith's hobby is tying 
         # fly-fishing lures)
-        inputbox.send_keys("Buy peackock feathers")
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
         # "1. Buy peacock feathers" as an item on the to-do list
@@ -42,14 +42,21 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = self.browser.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == "1. Buy peacock feathers" for row in rows),
-            "New to-do item did not appear in list"
-        )
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She enters "Use
         # peacock feahers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates and now she shes both items on her list
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = self.browser.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", 
+                      [row.text for row in rows])
 
         # Edith wonders whether the site remembers her list. Then she sees a that the
         # site has generated a URL for her -- there is some explanatory text to that 
@@ -58,6 +65,7 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL -- her to do list is still there
 
         # Satisfied, she goes back to sleep
-
+        self.fail('Finish the test!')
+        
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,13 @@ from .views import home_page
 
 class HomePageTest(TestCase):
     
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_POST_request(self):
+        response = self.client.post(
+            reverse("home"), 
+            data={"item_text": "A new to-do item"})
+        self.assertIn("A new to-do item", response.content.decode("utf8"))
         self.assertTemplateUsed(response, 'home.html')
