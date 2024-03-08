fdf336ce36a703f771cfc174c1e6ec2a2645a43a
Jason Tumusiime <jason.2mcme@gmail.com>
2024-03-08 08:20:40 +0300

    Add model to associate each item with different list

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from .models import Item
-from .views import home_page
+from .models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -10,23 +9,34 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, 'home.html')
-        
-class ItemModelTest(TestCase):
+
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Second list item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         self.assertEqual(saved_items[0].text, "The first (ever) list item")
+        self.assertEqual(saved_items[0].list, list_)
         self.assertEqual(saved_items[1].text, "Second list item")
+        self.assertEqual(saved_items[1].list, list_)
+
 
 class ListViewTest(TestCase):
     
@@ -35,8 +45,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response,'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        list_ = List.objects.create()
+        Item.objects.create(text="itemey 1", list=list_)
+        Item.objects.create(text="itemey 2", list=list_)
 
         response = self.client.get('/lists/the-only-list/')
 
