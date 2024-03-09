256897afe8ac89bbbb82af565d39225e91ba8a4c
Jason Tumusiime <jason.2mcme@gmail.com>
2024-03-09 14:47:51 +0300

    Add initial FT for item validation

--- functional_tests/test_item_validation.py
+++ functional_tests/test_item_validation.py
@@ -1,25 +1,40 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from unittest import skip
 
 from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
 
-    @skip
     def test_cannot_add_an_empty_list_item(self):
         # Edith goes to the homepage and tries to enter and empty list item.
         # She hit Enter on the empty input box
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
 
         # The homepage refreshes and there is an error message showing that the
         # list item cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CLASS_NAME, 'has-error').text,
+            "You cannot have an empty list item"
+        ))
 
         # She re-tries with some text for the item, which bow works
+        self.browser.find_element(By.ID, 'id_new_item').send_keys("Buy coffee")
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table("1: Buy coffee")
 
         # Perversely, she decided to submit another blank list item
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
 
         #She receives a similar warning on the list page
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CLASS_NAME, 'has-error').text,
+            "You cannot have an empty list item"
+        ))
 
         # And she can correct it by filling in some text
-        self.fail('write me!') 
+        self.browser.find_element(By.ID, 'id_new_item').send_keys("Make a cappucino")
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table("1: Buy coffee")
+        self.check_for_row_in_list_table("2: Make a cappucino")
